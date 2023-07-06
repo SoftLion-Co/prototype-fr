42aaa5565253ac4c6540b027596251e95360a509
Yurii <tahkict.yup4ik@gmail.com>
2023-07-06 21:56:05 +0300

    feature: add initial file structure
    Add initial file structure:
    -components files
    -links
    -route folders structure
    -modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link'
+
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className='flexCenter'>Home</h1>
+            <h2>Home</h2>
+            <Link href="/blogs"><h2>Blog</h2></Link>
+            <Link href="/projects"><h2>Projects</h2></Link>
+            <Link href="/services"><h2>Services</h2></Link>
         </div>
     )
 }
