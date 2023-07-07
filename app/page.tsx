27b96b6e684672d5ad0bceff1208b45d593a7dda
Yurii <tahkict.yup4ik@gmail.com>
2023-07-07 20:22:03 +0300

    feature: modify tailwind.config.js and sections
    Add some registration/email sections and contact us section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,21 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Home = () => {
-    return (
-        <div>
-            <h1 className='flexCenter'>Home</h1>
-            <h2>Home</h2>
-            <Link href="/blogs"><h2>Blog</h2></Link>
-            <Link href="/projects"><h2>Projects</h2></Link>
-            <Link href="/services"><h2>Services</h2></Link>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1 className="flexCenter">Home</h1>
+      <Link href="/blogs">
+        <h2>Blog</h2>
+      </Link>
+      <Link href="/projects">
+        <h2>Projects</h2>
+      </Link>
+      <Link href="/services">
+        <h2>Services</h2>
+      </Link>
+      <button className="button">ACCEPT</button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,23 +6,55 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    screens: {
-      'mobile': '320px',
-      // => @media (min-width: 640px) { ... }
-
-      'tablet': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'desktop': '1600px',
-      // => @media (min-width: 1024px) { ... }
-    },
     extend: {
+      screens: {
+        'mobile': '320px',
+        'tablet': '768px',
+        'desktop': '1600px',
+      },
+      fontFamily: {
+        besley: ['Besley', 'serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'button-blue-gradient': 'linear-gradient(139deg, #AFDAED 0%, #84C6E2 100%)',
+      },
+      boxShadow: {
+        'button-blue': '0px 3px 32px 0px rgba(36, 141, 238, 0.33)',
+      },
+      spacing: {
+        '155': '155px',
+        '307': '307px',
+        '53': '53px',
+      },
+      colors: {
+        customBlue: {
+          100: '#AFDAED',
+          500: '#84C6E2',
+        },
+      },
+      borderRadius: {
+        '15': '15px',
+      },
+      fontSize: {
+        '15': '15px',
+        '7.294': '7.294px',
+      },
+      padding: {
+        '19': '19px',
+        '35': '35px',
+      },
+      lineHeight: {
+        '151': '151.682%',
+      },
+      letterSpacing: {
+        '0.219': '0.219px',
       },
     },
   },
   plugins: [],
-}
+};
