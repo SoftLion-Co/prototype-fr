37c6e768e5b51e331cc6818cdeacbcd0ade007f8
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-07-12 23:30:25 +0300

    feature: add mobile header
    Needs fixs:
    -btn color
    - picture folder. To use logo

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,12 +1,29 @@
-import s from "./HeaderComponent.module.scss"
+import s from "./HeaderComponent.module.scss";
 
 const HeaderComponent = () => {
-    return (
-        <div className={s.container}>
-            <p>HEDER </p>
+  return (
+    <header className={s.container}>
+      <div className={s.header}>
+        <div className={s.header__logo}></div>
+        <button className={s.header__btnBurger}>
+          <svg
+            width="17"
+            height="13"
+            viewBox="0 0 22 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M0 1H22" stroke="#858598"></path>
+            <path d="M0 8H22" stroke="#858598"></path>
+            <path d="M0 15H22" stroke="#858598"></path>
+          </svg>
+        </button>
+        <button className={s.header__btnHeader}>
+          <p className={s.header__btnText}>Login</p>
+        </button>
+      </div>
+    </header>
+  );
+};
 
-        </div>
-    )
-}
-
-export default HeaderComponent
+export default HeaderComponent;
