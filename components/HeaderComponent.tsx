f990877367741fddc998a83ff41d7974220cfe53
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-07-13 14:24:14 +0300

    feature: add header for tablet, laptop and desktop

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,10 +1,29 @@
+import Link from "next/link";
 import s from "./HeaderComponent.module.scss";
+import MainButtonComponent from "./MainButtonComponent";
 
 const HeaderComponent = () => {
   return (
     <header className={s.container}>
       <div className={s.header}>
         <div className={s.header__logo}></div>
+        <nav  className={s.header__navigation}>
+          <Link href="/Servise" className={s.header__link}>
+            Servise
+          </Link>
+          <Link href="/OurProjects" className={s.header__link}>
+            Our Projects
+          </Link>
+          <Link href="/Technologies" className={s.header__link}>
+            Technologies
+          </Link>
+          <Link href="/Blog" className={s.header__link}>
+            Blog
+          </Link>
+          <Link href="/ContuctUs" className={s.header__link}>
+            Contuct Us
+          </Link>
+        </nav>
         <button className={s.header__btnBurger}>
           <svg
             width="17"
@@ -18,9 +37,10 @@ const HeaderComponent = () => {
             <path d="M0 15H22" stroke="#858598"></path>
           </svg>
         </button>
-        <button className={s.header__btnHeader}>
+        <MainButtonComponent loading={false} className={s.header__btnHeader}>
           <p className={s.header__btnText}>Login</p>
-        </button>
+        </MainButtonComponent>
+       
       </div>
     </header>
   );
