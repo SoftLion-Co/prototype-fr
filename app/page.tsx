3ce9f33dbe6c38d26345af75057e15668bb5095d
Paul <pawel.graur47@gmail.com>
2023-07-14 15:45:27 +0300

    feat/Added cards, function rearrange cards and css styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-import Link from 'next/link';
-
-import { LargeServiceCardComponent } from '../components/services/LargeServiceCardComponent';
-import cardComponent from './../data/LargeServiceCardComponent.json';
+import Link from "next/link";
+import HeroSection from "./sections/home_page/HeroSection"
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Home</h1>
-
-      <LargeServiceCardComponent cardComponent={cardComponent}/>
-
       <Link href="/blogs">
         <h2>Blog</h2>
       </Link>
@@ -19,6 +14,7 @@ const Home = () => {
       <Link href="/services">
         <h2>Services</h2>
       </Link>
+      <HeroSection />
     </div>
   );
 };
