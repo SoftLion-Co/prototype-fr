aca3a3e8c57ea6b342c265e0c150ef028a825e50
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-07-17 11:47:22 +0300

    refactor contact-us

--- components/FormComponent.tsx
+++ components/FormComponent.tsx
@@ -1,9 +1,9 @@
 "use client";
-
 import { useForm } from "react-hook-form";
 import { SubmitHandler } from "react-hook-form";
 import s from "./FormComponent.module.scss";
 import HeadingComponent from "./HeadingComponent";
+import NextLink from "next/link";
 
 interface FormData {
   name: string;
@@ -21,9 +21,7 @@ const FormComponent = () => {
   return (
     <div className={s.our_form}>
       <div className={s.title}>
-      <HeadingComponent text="Contact Us" />
-        {/* <h3 className={s.title__right}>Contact Us</h3> */}
-        {/* <span className={s.title__line}></span> */}
+        <HeadingComponent text="Contact Us" />
         <p className={s.title__text}>
           Submit your request now, and we will get in touch with you to discuss
           the details and develop a customized solution. Together, we can create
@@ -81,7 +79,9 @@ const FormComponent = () => {
         </button>
         <p className={s.form__text}>
           By clicking on this button I agree to the{" "}
-          <span className={s.pr}>processing of personal data</span>
+          <NextLink href={"/privacy-policy"}>
+            <span className={s.pr}>processing of personal data</span>
+          </NextLink>
         </p>
       </form>
     </div>
