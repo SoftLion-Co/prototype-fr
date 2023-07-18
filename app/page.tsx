3463de2a54923e80ca1a9f5c8bc10d5f90af3143
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-07-18 20:34:43 +0300

    refactor: fix page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Link from "next/link";
-import OurServicesSection from "./sections/home_page/OurServicesSection";
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Home</h1>
-      <OurServicesSection/>
-
       <Link href="/blogs">
         <h2>Blog</h2>
       </Link>
