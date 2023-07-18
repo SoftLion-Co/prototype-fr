5a4a6b4f2a8354658933925b3f479cc287b52952
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-07-18 20:13:53 +0300

    fix: add card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import CustomAppsSection from "./sections/service_page/CustomAppsSection";
 import TitleServicesComponent from "@/components/services/TitleServicesComponent";
+import ServiceCardsComponent from "@/components/services/ServiceCardsComponent";
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Home</h1>
       <TitleServicesComponent num="02" text ="Custum Apps"/>
+      <ServiceCardsComponent arrow={true}/>
       {/* <CustomAppsSection /> */}
       <Link href="/blogs">
         <h2>Blog</h2>
--- components/services/ServiceCardsComponent.tsx
+++ components/services/ServiceCardsComponent.tsx
@@ -1,11 +1,38 @@
-import s from "./ServiceCardsComponent.module.scss"
+import s from "./ServiceCardsComponent.module.scss";
+import React, { FC, ReactNode, ComponentProps } from "react";
+import classNames from "classnames";
+import Link from "next/link";
+import phone from "../../images/services/custom-apps/Apps.svg";
+import { PiArrowRightThin } from "react-icons/pi";
+import Image from "next/image";
 
-const ServiceCardsComponent = () => {
-    return (
-        <div>
-
-        </div>
-    )
+interface ServiceCardsComponentProps {
+  arrow: boolean;
 }
 
-export default ServiceCardsComponent
+const ServiceCardsComponent: FC<ServiceCardsComponentProps> = ({
+  arrow = false,
+}) => {
+  return (
+    <div className={s.servicecard}>
+      <Image className={s.servicecard__phone} src={phone} alt="phone" />
+      <h2>App</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quasi
+        officiis ex, quam similique ullam dolore! Illum ab facere fuga sint, non
+        facilis exercitationem laboriosam rem? Consectetur illo deleniti magni.
+      </p>
+      {arrow?
+      <button>
+        <PiArrowRightThin
+          className={s.servicecard__arrow}
+          width="51px"
+          height="49px"
+        />
+      </button>
+      :null }
+    </div>
+  );
+};
+
+export default ServiceCardsComponent;
