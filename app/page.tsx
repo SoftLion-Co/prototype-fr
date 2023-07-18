d543d7f9b3edfbff283510b87626b0a773b6640e
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-07-18 23:32:47 +0300

    feat: ServiceCardsComponent mobile init

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,20 @@ import Link from "next/link";
 import CustomAppsSection from "./sections/service_page/CustomAppsSection";
 import TitleServicesComponent from "@/components/services/TitleServicesComponent";
 import ServiceCardsComponent from "@/components/services/ServiceCardsComponent";
+import pic from "../images/services/custom-apps/Apps.svg"
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Home</h1>
-      <TitleServicesComponent num="02" text ="Custum Apps"/>
-      <ServiceCardsComponent arrow={true}/>
+      <TitleServicesComponent num="02" text={"Custum Apps"} className="TitleServicesComponent" />
+      <ServiceCardsComponent
+        title="Apps"
+        path={pic}
+        text="App development is the creation of software for devices like smartphones, tablets, and computers. It involves building tools that perform tasks and provide users with capabilities. Apps are developed for platforms like iOS, Android, and Windows. This process includes UI design, programming, testing, debugging, and post-release support. App development is crucial for technology, enabling innovative solutions to meet user needs.
+"
+arrow={true}
+      />
       {/* <CustomAppsSection /> */}
       <Link href="/blogs">
         <h2>Blog</h2>
--- components/services/ServiceCardsComponent.tsx
+++ components/services/ServiceCardsComponent.tsx
@@ -1,29 +1,31 @@
 import s from "./ServiceCardsComponent.module.scss";
 import React, { FC, ReactNode, ComponentProps } from "react";
-import classNames from "classnames";
-import Link from "next/link";
-import phone from "../../images/services/custom-apps/Apps.svg";
+// import pic from "../../images/services/custom-apps/Apps.svg";
 import { PiArrowRightThin } from "react-icons/pi";
 import Image from "next/image";
 
 interface ServiceCardsComponentProps {
-  arrow: boolean;
+  arrow?: boolean;
+  path?: any;
+  title?: string;
+  text?: string;
 }
 
 const ServiceCardsComponent: FC<ServiceCardsComponentProps> = ({
   arrow = false,
+  path,
+  title,
+  text,
 }) => {
   return (
     <div className={s.servicecard}>
-      <Image className={s.servicecard__phone} src={phone} alt="phone" />
-      <h2>App</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quasi
-        officiis ex, quam similique ullam dolore! Illum ab facere fuga sint, non
-        facilis exercitationem laboriosam rem? Consectetur illo deleniti magni.
+      <Image className={s.servicecard__pic} src={path} alt="phone"  />
+      <h2 className={s.servicecard__title}>{title}</h2>
+      <p className={s.servicecard__text}>
+    {text}
       </p>
       {arrow?
-      <button>
+      <button className={s.servicecard__btnArrow}>
         <PiArrowRightThin
           className={s.servicecard__arrow}
           width="51px"
