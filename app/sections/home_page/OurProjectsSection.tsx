b4100a3a764fbaef2aea293578b040c7c956afbb
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-07-19 14:13:58 +0300

    refactor: fix project card and projects section

--- app/sections/home_page/OurProjectsSection.tsx
+++ app/sections/home_page/OurProjectsSection.tsx
@@ -2,13 +2,37 @@ import s from "./OurProjectsSection.module.scss"
 import ProjectHeadingComponent from "./../../../components/projects/ProjectHeadingComponent"
 import ProjectCardComponent from "./../../../components/projects/ProjectCardComponent"
 
+const sampleData = [
+    {
+      id: 1,
+      image: "https://img.freepik.com/free-photo/creative-copywriting-commercial-text-seo-editing_107791-15687.jpg?w=2000&t=st=1689761442~exp=1689762042~hmac=764807ff7ce02e9377fd4137c6e335816a5e066d40244b6dd7f13aca2a9fd9b9",
+      title: "Landing page of traveling company",
+      customer: "customer: Move",
+      year: "year: 2022",
+      author: "author: SoftLion",
+      description:
+        "In this project, we developed the design, implemented it, and provide further support",
+    },
+    {
+        id: 2,
+        image: "https://img.freepik.com/free-photo/creative-copywriting-commercial-text-seo-editing_107791-15687.jpg?w=2000&t=st=1689761442~exp=1689762042~hmac=764807ff7ce02e9377fd4137c6e335816a5e066d40244b6dd7f13aca2a9fd9b9",
+        title: "Landing page of traveling company",
+        customer: "customer: Move",
+        year: "year: 2022",
+        author: "author: SoftLion",
+        description:
+          "In this project, we developed the design, implemented it, and provide further support",
+      },
+  ];
+
 const OurProjectsSection = () => {
     return (
         <div>
-            <ProjectHeadingComponent />
-            <ProjectCardComponent />
+          {sampleData.map((data) => (
+            <ProjectCardComponent key={data.id} data={data} />
+          ))}
         </div>
-    )
+      )
 }
 
 export default OurProjectsSection
--- components/projects/ProjectCardComponent.tsx
+++ components/projects/ProjectCardComponent.tsx
@@ -2,39 +2,23 @@ import React from "react";
 import s from "./ProjectCardComponent.module.scss";
 import Image from "next/image";
 
-const sampleData = [
-  {
-    id: 1,
-    image: "/path/to/image1.jpg",
-    title: "Landing page of traveling company",
-    customer: "customer: Move",
-    year: "year: 2022",
-    author: "author: SoftLion",
-    description: "In this project, we developed the design, implemented it, and provide further support",
-  },
-];
-
-const ProjectCardComponent = () => {
+const ProjectCardComponent = ({ data }) => {
   return (
     <div className={s.card}>
-      {sampleData.map((data) => (
-        <div key={data.id}>
-          <div className={s.card__main}>
-            <Image src={data.image} alt="Project Image" width={300} height={200} />
-            <h4>{data.title}</h4>
-          </div>
-          <div className={s.card__submain}>
-            <div className={s.card__subinfo}>
-              <p className={s.card__customer}>{data.customer}</p>
-              <p className={s.card__year}>{data.year}</p>
-              <p className={s.card__author}>{data.author}</p>
-            </div>
-            <div className={s.card__subdesc}>
-              <p>{data.description}</p>
-            </div>
-          </div>
+      <div className={s.card__main}>
+        <Image className={s.card__image} src={data.image} alt="Project Image" width={16000} height={19000} />
+        <h4 className={s.card__title}>{data.title}</h4>
+      </div>
+      <div className={s.card__submain}>
+        <div className={s.card__subinfo}>
+          <p className={s.card__customer}>{data.customer}</p>
+          <p className={s.card__year}>{data.year}</p>
+          <p className={s.card__author}>{data.author}</p>
         </div>
-      ))}
+        <div className={s.card__subdesc}>
+          <p>{data.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
