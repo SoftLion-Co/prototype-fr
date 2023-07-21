5d3e6e05e299169075881624aea35e57406e185e
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-07-21 13:55:53 +0300

    feature: section security

--- app/sections/service_page/SecuritySection.tsx
+++ app/sections/service_page/SecuritySection.tsx
@@ -1,41 +1,63 @@
-'use client'
+"use client";
 
 import s from "./SecuritySection.module.scss";
 import ServiceSecurityCardComponent from "@/components/service/ServiceSecurityCardComponent";
 import MobileSliderComponent from "@/components/MobileSliderComponent";
+import ServiceHeadingComponent from "@/components/service/ServiceHeadingComponent";
 
 const securityCardsData = [
   {
     id: 1,
-    title: "Card 1",
-    description: "This is the description for Card 1.",
+    title: "Discovery Stage",
+    description:
+      "When developing applications or websites, we pay great attention to protecting the customer's confidential information. We employ advanced encryption technologies to safeguard personal data and other sensitive information.",
   },
   {
     id: 2,
-    title: "Card 2",
-    description: "This is the description for Card 2.",
+    title: "Protection",
+    description:
+      "During this stage, the focus is on understanding the client's needs and requirements. This involves conducting research, gathering information, and analyzing the client's business goals.",
   },
   {
     id: 3,
-    title: "Card 3",
-    description: "This is the description for Card 3.",
+    title: "Security Measures",
+    description:
+      "We implement various security measures, such as protection against hacking attacks, software vulnerability protection, and abuse prevention. We regularly update systems and perform security audits to ensure the highest level of customer data protection.",
   },
   {
     id: 4,
-    title: "Card 4",
-    description: "This is the description for Card 4.",
+    title: "Use of Secure Data Transmission Protocols",
+    description:
+      "We utilize secure data transmission protocols, such as HTTPS, to encrypt and protect information transmitted between users and the application or website. This helps prevent interception of information during transmission.",
   },
   {
     id: 5,
-    title: "Card 5",
-    description: "This is the description for Card 5.",
+    title: "Reliable Servers and Hosting Providers",
+    description:
+      "We collaborate with trusted server and hosting providers that ensure a high level of data protection and system reliability. This helps minimize the risk of data loss or damage.",
   },
 ];
 
 const SecuritySection = () => {
   return (
-    <div>
-      <MobileSliderComponent data={securityCardsData} SlideComponent={ServiceSecurityCardComponent}/>
+    <div className={s.container}>
+      <ServiceHeadingComponent headingText="Security" />
+      <div className={s.security__slider}>
+        <MobileSliderComponent
+          data={securityCardsData}
+          SlideComponent={ServiceSecurityCardComponent}
+        />
+      </div>
+
+      <div className={s.security__container}>
+        {securityCardsData.map((card) => (
+          <ServiceSecurityCardComponent
+            key={card.id}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- components/service/ServiceSecurityCardComponent.tsx
+++ components/service/ServiceSecurityCardComponent.tsx
@@ -2,20 +2,29 @@ import s from "./ServiceSecurityCardComponent.module.scss";
 import Image from "next/image";
 import Security from "../../images/security.svg";
 
-const ServiceSecurityCardComponent = () => {
+interface ServiceSecurityCardComponentProps {
+  title?: string;
+  description?: string;
+  data?: { description: string; title: string }; // Combine both description and title properties inside data
+}
+
+const ServiceSecurityCardComponent: React.FC<
+  ServiceSecurityCardComponentProps
+> = ({ title, description, data }) => {
+  const cardTitle = title || (data && data.title);
+  const cardDescription = description || (data && data.description);
 
   return (
     <div className={s.security}>
       <div className={s.security__head}>
-        <Image src={Security} alt="Security Cards" className={s.security__img}/>
-        <h4 className={s.security__title}>Discovery Stage</h4>
+        <Image
+          src={Security}
+          alt="Security Cards"
+          className={s.security__img}
+        />
+        <h4 className={s.security__title}>{cardTitle}</h4>
       </div>
-      <p className={s.security__text}>
-        When developing applications or websites, we pay great attention to
-        protecting the customer's confidential information. We employ advanced
-        encryption technologies to safeguard personal data and other sensitive
-        information.
-      </p>
+      <p className={s.security__text}>{cardDescription}</p>
     </div>
   );
 };
