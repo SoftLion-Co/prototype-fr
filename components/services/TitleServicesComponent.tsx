b1d4f88cebb3459b46e71fe99bad85af810eb730
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-07-24 12:24:37 +0300

    fix: titleServicesComponent add position

--- components/services/TitleServicesComponent.tsx
+++ components/services/TitleServicesComponent.tsx
@@ -1,53 +1,33 @@
 import s from "./TitleServicesComponent.module.scss";
 import React, { FC } from "react";
+import classNames from "classnames";
 
 interface TitleServicesComponentProps {
-  num: number | string;
+  num: string;
   text: string;
+  position?: "left" | "right";
+  className:string;
 }
 
 const TitleServicesComponent: FC<TitleServicesComponentProps> = ({
   num,
   text,
+  position="left",
+  className,
 }) => {
+
+  const buttonClass = classNames(s.title__num, className, {
+    [s.left]: position === "left",
+    [s.right]: position === "right",
+  });
+
+
   return (
     <div className={s.title}>
-      <h1 className={s.title__num}>{num}</h1>
+      <h1 className={buttonClass}>{num}</h1>
       <h2 className={s.title__text}>{text}</h2>
     </div>
   );
 };
 
 export default TitleServicesComponent;
-
-// import s from "./TitleServicesComponent.module.scss";
-// import React, { FC } from "react";
-// import classNames from "classnames";
-
-// interface TitleServicesComponentProps {
-//   num: string;
-//   text: string;
-//   position?: "left" | "right";
-// }
-
-// const TitleServicesComponent: FC<TitleServicesComponentProps> = ({
-//   num,
-//   text,
-//   position="left",
-// }) => {
-
-//   const buttonClass = classNames(s.titlenum, className, {
-//     [s.left]: position === "left",
-//     [s.right]: position === "right",
-//   });
-
-
-//   return (
-//     <div className={s.title}>
-//       <h1 className={buttonClass}>{num}</h1>
-//       <h2 className={s.titletext}>{text}</h2>
-//     </div>
-//   );
-// };
-
-// export default TitleServicesComponent;
