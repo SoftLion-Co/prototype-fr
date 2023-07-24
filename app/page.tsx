c4c76549a452f97fd71f48039fc75e2e3f14c9ba
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-07-24 11:13:01 +0300

    refactor: size card tablet

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import SecuritySection from "./sections/service_page/SecuritySection";
 
 const Home = () => {
   return (
@@ -13,6 +14,7 @@ const Home = () => {
       <Link href="/services">
         <h2>Services</h2>
       </Link>
+      <SecuritySection/>
     </div>
   );
 };
