68542ef6aaffc38717d03396057a5d0b5efee17a
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-07-25 18:56:17 +0300

    feature: add desktop OurProjectsSection

--- components/projects/ProjectCardComponent.tsx
+++ components/projects/ProjectCardComponent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./ProjectCardComponent.module.scss";
 import Image from "next/image";
+import { PiArrowRightThin } from "react-icons/pi";
 
 interface ProjectData {
   id: number;
@@ -12,25 +13,34 @@ interface ProjectData {
   description: string;
 }
 
-const ProjectCardComponent: React.FC<{ data: ProjectData }> = ({ data }) => {
+const ProjectMobileCardComponent: React.FC<{ data: ProjectData }> = ({
+  data,
+}) => {
   return (
     <div className={s.card}>
-      <div className={s.card__main}>
-        <Image className={s.card__image} src={data.image} alt="Project Image" width={16000} height={19000} />
-        <h4 className={s.card__title}>{data.title}</h4>
-      </div>
-      <div className={s.card__submain}>
+      <div className={s.card__info}>
+        <p className={s.card__customer}>{data.customer}</p>
         <div className={s.card__subinfo}>
-          <p className={s.card__customer}>{data.customer}</p>
           <p className={s.card__year}>{data.year}</p>
           <p className={s.card__author}>{data.author}</p>
         </div>
-        <div className={s.card__subdesc}>
-          <p>{data.description}</p>
+      </div>
+      <Image
+        className={s.card__image}
+        src={data.image}
+        alt="Project Image"
+        width={16000}
+        height={19000}
+      />
+      <div className={s.card__text}>
+        <div className={s.card__subtext}>
+          <h4 className={s.card__title}>{data.title}</h4>
+          <p className={s.card__desc}>{data.description}</p>
         </div>
+        <PiArrowRightThin className={s.card__arrowIcon} />
       </div>
     </div>
   );
 };
 
-export default ProjectCardComponent;
+export default ProjectMobileCardComponent;
