b34c79993d9226b5f3f2e666ea4ecb0356bd26df
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-07-26 13:29:49 +0300

    refactor: remove padding from title

--- app/sections/home_page/OurServicesSection.tsx
+++ app/sections/home_page/OurServicesSection.tsx
@@ -13,7 +13,6 @@ const OurServicesSection = () => {
     <div className={s.services}>
       <OurServicesHeadingComponent />
       <CustomAppsSection />
-      <CustomAppsSection />
     </div>
   );
 };
