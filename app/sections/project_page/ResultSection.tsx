0ad77c13ced8f122d2312375a4aeb67508f4ec9d
mafioz359 <mishazlupko@gmail.com>
2023-07-27 19:21:57 +0300

    refactor: add array width information

--- app/sections/project_page/ResultSection.tsx
+++ app/sections/project_page/ResultSection.tsx
@@ -2,35 +2,35 @@ import s from "./ResultSection.module.scss";
 import Image from "next/image";
 import Photo from "../../../images/project/Vectary-texture.svg";
 
+const info = [
+  {
+    titleFirst:
+      "Attractive and modern design: We have developed an aesthetically appealing design that reflects the unique style of your company and captures the attention of visitors. The website has a clean and organized layout that ensures easy navigation and user-friendliness.",
+  },
+  {
+    titleSecond:
+      "SEO optimization: We have taken into account search engine optimization (SEO) principles during the website development process.",
+  },
+  {
+    titleThird:
+      "Our website is fully responsive and compatible with various devices such as smartphones and tablets. Your customers will have access to information and booking capabilities from any device, ensuring convenience and accessibility.",
+  },
+];
+
 const ResultSection = () => {
   return (
     <div className={`${s.container} ${s.design}`}>
       <ul className={s.design__texts}>
         <li className={s.design__listFirst}>
-          <p className={s.design__text}>
-            Attractive and modern design: We have developed an aesthetically
-            appealing design that reflects the unique style of your company and
-            captures the attention of visitors. The website has a clean and
-            organized layout that ensures easy navigation and user-friendliness.
-          </p>
+          <p className={s.design__text}>{info[0].titleFirst}</p>
         </li>
         <li className={s.design__listSecond}>
-          <p className={s.design__text}>
-            SEO optimization: We have taken into account search engine
-            optimization (SEO) principles during the website development
-            process.
-          </p>
+          <p className={s.design__text}>{info[1].titleSecond}</p>
         </li>
         <li className={s.design__listThird}>
-          <p className={s.design__text}>
-            Our website is fully responsive and compatible with various devices
-            such as smartphones and tablets. Your customers will have access to
-            information and booking capabilities from any device, ensuring
-            convenience and accessibility.
-          </p>
+          <p className={s.design__text}>{info[2].titleThird}</p>
         </li>
       </ul>
-      {/* <div className={s.design__photo}></div> */}
       <Image src={Photo} alt="" className={s.design__photo} />
     </div>
   );
