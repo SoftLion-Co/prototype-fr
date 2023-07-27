33c41e5953310d48ce598717ddc01eda926afaa1
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-07-27 15:15:19 +0300

    Update ProjectCardComponent.tsx

--- components/projects/ProjectCardComponent.tsx
+++ components/projects/ProjectCardComponent.tsx
@@ -5,31 +5,22 @@ import { PiArrowRightThin } from "react-icons/pi";
 import Link from "next/link";
 
 interface ProjectData {
-  data: {
-    id: number;
-    image: string;
-    title: string;
-    customer: string;
-    year: string;
-    author: string;
-    description: string;
-  };
+  id: number;
+  image: string;
+  title: string;
+  customer: string;
+  year: string;
+  author: string;
+  description: string;
 }
 
-const ProjectCardComponent: React.FC<ProjectData> = ({ data }) => {
+const ProjectCardComponent: React.FC<{ data: ProjectData }> = ({
+  data,
+}) => {
   return (
     <div className={s.card}>
-      <div className={s.card__main}>
-        <Image
-          className={s.card__image}
-          src={data.image}
-          alt="Project Image"
-          width={16000}
-          height={19000}
-        />
-        <h4 className={s.card__title}>{data.title}</h4>
-      </div>
-      <div className={s.card__submain}>
+      <div className={s.card__info}>
+        <p className={s.card__customer}>{data.customer}</p>
         <div className={s.card__subinfo}>
           <p className={s.card__year}>{data.year}</p>
           <p className={s.card__author}>{data.author}</p>
