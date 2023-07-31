4225be6d1b2c11c320a010612c1d47333e19a601
zhuvakAnd <zhuvak.andrii@chnu.edu.ua>
2023-07-31 12:12:08 +0300

    refactor: mobile card

--- components/blog/BlogMobileExtendedCardComponent.tsx
+++ components/blog/BlogMobileExtendedCardComponent.tsx
@@ -2,20 +2,31 @@ import s from "./BlogMobileExtendedCardComponent.module.scss";
 
 const BlogMobileExtendedCardComponent: React.FC<any> = ({ data }) => {
   return (
-    <div className={s.articleCard}>
-      <div className={s.articleCard__content}>
+    <div className={s.card}>
+      <div className={s.card__content}>
         <img
-          className={s.articleCard__content__image}
+          className={s.card__content__image}
           src={data.imageSrc}
           alt="Softlion image"
         />
-        <h2 className={s.articleCard__content__title}>{data.title}</h2>
+        <div className={s.info}>
+          <h2 className={s.info__title}>{data.title}</h2>
+          <div className={s.info__author}>
+            <img
+              className={s.info__author__icon}
+              src={data.authorIconSrc}
+              alt="Softlion image"
+            />
+            <p className={s.info__author__name}>{data.author}</p>
+          </div>
+          <p className={s.info__time}>Reading time: {data.readingTime}</p>
+        </div>
       </div>
-      <div className={s.articleCard__line}></div>
-      <div className={s.articleCard__article}>
-        <p className={s.articleCard__article__text}>{data.text}</p>
+      <div className={s.card__line}></div>
+      <div className={s.card__article}>
+        <p className={s.card__article__text}>{data.text}</p>
         <svg
-          className={s.articleCard__article__arrow}
+          className={s.card__article__arrow}
           xmlns="http://www.w3.org/2000/svg"
           width="3em"
           height="3em"
