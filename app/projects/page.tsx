eebae06381b942f063f0b070f744170c4e19e390
mafioz359 <mishazlupko@gmail.com>
2023-07-31 14:29:41 +0300

    feature: add solution, but don't finish

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,13 @@
-import s from "./page.module.scss"
+import s from "./page.module.scss";
+import SolutionSection from "../sections/project_page/SolutionSection";
 
 const Projects = () => {
-    return (
-        <div>
-            <h1>Projects</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Projects</h1>
+      <SolutionSection />
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
--- app/sections/project_page/SolutionSection.tsx
+++ app/sections/project_page/SolutionSection.tsx
@@ -1,11 +1,40 @@
-import s from "./SolutionSection.module.scss"
+import s from "./SolutionSection.module.scss";
+
+const info = [
+  {
+    description:
+      "Upon receiving your request, we proceed to review and analyze all the details. Our experts study your requirements and expectations, conducting additional research if necessary, to ensure successful project implementation.",
+  },
+  {
+    title: "Analytics and Tracking",
+    description:
+      "Integrated robust analytics and tracking tools, such as Google Analytics, to collect valuable data on website visits, user behavior, and conversions. Utilized this data to gain insights into user preferences, measure the effectiveness of marketing strategies, and make data-driven decisions for further improvements.",
+  },
+  {
+    title: "Fast Page Loading",
+    description:
+      "Implemented various optimization techniques to enhance page loading speed and performance. This included optimizing image sizes, minimizing file sizes, leveraging caching mechanisms, and utilizing content delivery networks (CDNs) to deliver content efficiently. As a result, visitors experience fast and responsive page loading, ensuring a positive user experience.",
+  },
+  {
+    title: "Support and After-Sales Service",
+    description:
+      "Established a reliable support system to address customer inquiries, concerns, and provide assistance throughout the customer journey. Implemented live chat support, email ticketing systems, and a dedicated customer support team to ensure prompt and efficient resolution of customer issues, fostering reliable cooperation and customer satisfaction. ",
+  },
+];
 
 const SolutionSection = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default SolutionSection
+  return (
+    <section className={`${s.container} ${s.solution}`}>
+      <ul className={s.solution__list}>
+        {info.map((item, index) => (
+          <li key={index} className={s.solution__item}>
+            <h3 className={s.solution__heading}>{item.title}</h3>
+            <p className={s.solution__text}>{item.description}</p>
+          </li>
+        ))}
+      </ul>
+      <ul></ul>
+    </section>
+  );
+};
+export default SolutionSection;
