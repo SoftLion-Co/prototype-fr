edcc7b5c1a8cd8bd7d718e2793a7aa9158c36322
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-08-02 22:49:56 +0300

    fix conflicts

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,25 +1,20 @@
 import s from "./page.module.scss";
 import CustomAppsSection from "../sections/service_page/CustomAppsSection";
-<<<<<<< HEAD
+
 import DevelopmentSection from "../sections/service_page/DevelopmentSection";
-=======
 
 import CustomAppsCardComponent from "@/components/services/CustomAppsCardComponent";
 import SecuritySection from "../sections/service_page/SecuritySection";
 
->>>>>>> 9cda237a163f55c2e887875c6fa7e24ef98f9cdc
+
 
 const Services = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <p>Services</p>
-      <DevelopmentSection />
-      <CustomAppsSection />
-=======
+
      <CustomAppsSection/>
      <SecuritySection />
->>>>>>> 9cda237a163f55c2e887875c6fa7e24ef98f9cdc
+
     </div>
   );
 };
