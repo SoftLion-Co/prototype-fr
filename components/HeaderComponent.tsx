8b9b013bfb825c9bf7517a5ed0de13cf3fbd490f
Yurii <tahkict.yup4ik@gmail.com>
2023-08-04 20:35:43 +0300

    refactor: fix header grammar

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -19,7 +19,7 @@ const HeaderComponent = () => {
        </Link>
         <nav  className={s.header__navigation}>
           <Link href="/services" className={s.header__link}>
-            Servise
+            Services
           </Link>
           <Link href="/projects" className={s.header__link}>
             Our Projects
