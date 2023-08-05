dab079047e8c385b6587d0f7f0da6c6b56c0dd5c
zhuvakAnd <zhuvak.andrii@chnu.edu.ua>
2023-08-05 10:43:30 +0300

    feature: project description

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,12 @@
 import s from "./page.module.scss";
 import ResultSection from "../sections/project_page/ResultSection";
+import ProjectDescriptionSection from "../sections/project_page/ProjectDescriptionSection";
 
 const Projects = () => {
   return (
     <div>
       <h1>Projects</h1>
+      <ProjectDescriptionSection />
       <ResultSection />
     </div>
   );
