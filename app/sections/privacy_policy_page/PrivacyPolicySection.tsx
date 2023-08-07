ba4ef9d274bcb0691da83d51dc9569d9293d21ce
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-08-07 23:08:24 +0300

    init: add text

--- app/sections/privacy_policy_page/PrivacyPolicySection.tsx
+++ app/sections/privacy_policy_page/PrivacyPolicySection.tsx
@@ -1,8 +1,22 @@
-import s from "./PrivacyPolicySection.module.scss"
+// import s from "./PrivacyPolicySection.module.scss"
 
 const PrivacyPolicySection = () => {
     return (
         <div>
+            <p><b>SUMMARY OF THE KEY POINTS</b>
+
+<b>In Short:</b> This summary highlights important points from our privacy notice, which applies to SoftLion. For more detailed information on any of these topics, please click the corresponding link or use our table of contents to find the relevant section.
+
+What personal information do we collect? We collect personal information based on your interactions with SoftLion and our Services, as well as the choices you make and the features you use. Please click here to learn more.
+Do we process sensitive personal information? No, we don’t process sensitive personal information.
+Do we receive information from third parties? No, we don’t receive information from third parties.
+How do we process your information? We process your information to provide, improve, and manage our Services, communicate with you, prevent security breaches and fraud, and comply with legal requirements. With your consent, we may also process your information for other purposes. We process your information only when it is legally permitted to do so. Please click here to learn more.
+In what circumstances do we share personal information and with whom? We may share personal information in specific circumstances and with specific categories of third parties. Please click here to learn more.
+How do we safeguard your information? We have technical and organizational measures in place to protect your personal information. However, there is no entirely secure system yet, so we cannot guarantee that unauthorized third parties will not be able to access your information. Please click here to learn more.
+What are your rights? Depending on your location, you may have certain rights regarding your personal information under applicable privacy laws. Please click here to learn more.
+How do you exercise your rights? The simplest way to exercise your rights is by completing our data subject request form, available here, or by contacting us. We will review and address any request in compliance with applicable data protection laws.
+To learn more about how SoftLion handles the information we collect, please click here to read our full privacy notice.
+</p>
             
         </div>
     )
