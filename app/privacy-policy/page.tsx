530b8088964937b953f9305fc7fb02520f92b216
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-08-09 22:53:23 +0300

    chore: mobile init

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,9 +1,11 @@
 import s from "./page.module.scss"
+import PrivacyPolicySection from "../sections/privacy_policy_page/PrivacyPolicySection"
 
 const PrivacyPolicy = () => {
     return (
         <div>
             <h1>Privacy-policy</h1>
+            <PrivacyPolicySection />
         </div>
     )
 }
