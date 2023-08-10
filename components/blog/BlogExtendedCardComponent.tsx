9818086aa956e3d75a4f228a7f7730752ad69923
zhuvakAnd <125382431+zhuvakAnd@users.noreply.github.com>
2023-08-10 13:52:52 +0300

    add blog links

--- components/blog/BlogExtendedCardComponent.tsx
+++ components/blog/BlogExtendedCardComponent.tsx
@@ -25,7 +25,6 @@ const BlogExtendedCardComponent: React.FC<BlogExtendedCardComponentProps> = ({
   imageSrc,
   tags,
 }) => {
-  console.log(tags);
   return (
     <div className={s.card}>
       <div className={s.card__container}>
@@ -56,22 +55,24 @@ const BlogExtendedCardComponent: React.FC<BlogExtendedCardComponentProps> = ({
       <div className={s.card__line}></div>
       <div className={s.article}>
         <p className={s.card__text}>{text}</p>
-        <svg
-          className={s.card__arrow}
-          xmlns="http://www.w3.org/2000/svg"
-          width="5em"
-          height="5em"
-          viewBox="0 0 20 20"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="4%"
-        >
-          <path
-            d="M5 10h9m0 0l-4-4m4 4l-4 4"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        <Link href={`blogs/${id}`}>
+          <svg
+            className={s.card__arrow}
+            xmlns="http://www.w3.org/2000/svg"
+            width="5em"
+            height="5em"
+            viewBox="0 0 20 20"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="4%"
+          >
+            <path
+              d="M5 10h9m0 0l-4-4m4 4l-4 4"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </Link>
       </div>
     </div>
   );
