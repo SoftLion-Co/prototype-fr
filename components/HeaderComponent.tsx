2f8c8d889dbb0931e964721a495fca2bd12fc8d9
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-08-15 20:09:21 +0300

    feature:add  submenu

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -3,6 +3,7 @@ import s from "./HeaderComponent.module.scss";
 import { IoIosMenu } from "react-icons/io";
 import Logo from "images/logo.svg";
 import Image from "next/image";
+import classNames from "classnames";
 
 const HeaderComponent = () => {
   return (
@@ -12,21 +13,95 @@ const HeaderComponent = () => {
           <Image className={s.header__logo} src={Logo} alt="SoftLion" />
         </Link>
         <nav className={s.header__navigation}>
-          <Link href="/services" className={s.header__link}>
-            Services
-          </Link>
-          <Link href="/projects" className={s.header__link}>
-            Our Projects
-          </Link>
-          <Link href="/technologies" className={s.header__link}>
-            Technologies
-          </Link>
-          <Link href="/blogs" className={s.header__link}>
-            Blog
-          </Link>
-          <Link href="/contact-us" className={s.header__link}>
-            Contact Us
-          </Link>
+          <ul className={s.header__list}>
+            <li>
+              {" "}
+              <Link href="/services" className={s.header__link}>
+                Services
+              </Link>
+              <ul className={s.header__subMenu}>
+                <li>
+                  {" "}
+                  <Link href="/services/design" className={s.header__subLink}>
+                    Design
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link
+                    href="/services/development"
+                    className={s.header__subLink}
+                  >
+                    Development
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/apps" className={s.header__subLink}>
+                    Apps
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              {" "}
+              <Link href="/projects" className={s.header__link}>
+                Our Projects
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link href="/technologies" className={s.header__link}>
+                Technologies
+              </Link>
+              <ul className={s.header__subMenu}>
+                <li>
+                  {" "}
+                  <Link href="/services/java" className={s.header__subLink}>
+                    Java
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link href="/services/react" className={s.header__subLink}>
+                    React.js
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/angular" className={s.header__subLink}>
+                    Angular
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/vue" className={s.header__subLink}>
+                    Vue.js
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/node" className={s.header__subLink}>
+                    Node.js
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/net" className={s.header__subLink}>
+                    .NET
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              {" "}
+              <Link href="/blogs" className={s.header__link}>
+                Blog
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link href="/contact-us" className={s.header__link}>
+                Contact Us
+              </Link>
+            </li>
+          </ul>
         </nav>
         <button className={s.header__btnBurger}>
           <IoIosMenu className={s.header__burger} width="18px" height="13px" />
