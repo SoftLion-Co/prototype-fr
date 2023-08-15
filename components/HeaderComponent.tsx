f931e5a9d200fac19dd1634f5d9b46668e26d967
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-08-15 23:06:32 +0300

    refactor: sub menu

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -14,88 +14,84 @@ const HeaderComponent = () => {
         </Link>
         <nav className={s.header__navigation}>
           <ul className={s.header__list}>
-            <li>
+            <li className={s.header__item}>
               {" "}
               <Link href="/services" className={s.header__link}>
                 Services
               </Link>
-              <ul className={s.header__subMenu}>
+              <ul className={s.header__sub_menu}>
                 <li>
                   {" "}
-                  <Link href="/services/design" className={s.header__subLink}>
+                  <Link href="/services/design" className={s.header__link}>
                     Design
                   </Link>
                 </li>
                 <li>
                   {" "}
-                  <Link
-                    href="/services/development"
-                    className={s.header__subLink}
-                  >
+                  <Link href="/services/development" className={s.header__link}>
                     Development
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services/apps" className={s.header__subLink}>
+                  <Link href="/services/apps" className={s.header__link}>
                     Apps
                   </Link>
                 </li>
               </ul>
             </li>
-
-            <li>
+            <li className={s.header__item}>
               {" "}
               <Link href="/projects" className={s.header__link}>
                 Our Projects
               </Link>
             </li>
-            <li>
+            <li className={s.header__item}>
               {" "}
               <Link href="/technologies" className={s.header__link}>
                 Technologies
               </Link>
-              <ul className={s.header__subMenu}>
-                <li>
+              <ul className={s.header__sub_menu}>
+                <li >
                   {" "}
-                  <Link href="/services/java" className={s.header__subLink}>
+                  <Link href="/services/java" className={s.header__link}>
                     Java
                   </Link>
                 </li>
                 <li>
                   {" "}
-                  <Link href="/services/react" className={s.header__subLink}>
+                  <Link href="/services/react" className={s.header__link}>
                     React.js
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services/angular" className={s.header__subLink}>
+                  <Link href="/services/angular" className={s.header__link}>
                     Angular
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services/vue" className={s.header__subLink}>
+                  <Link href="/services/vue" className={s.header__link}>
                     Vue.js
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services/node" className={s.header__subLink}>
+                  <Link href="/services/node" className={s.header__link}>
                     Node.js
                   </Link>
                 </li>
                 <li>
-                  <Link href="/services/net" className={s.header__subLink}>
+                  <Link href="/services/net" className={s.header__link}>
                     .NET
                   </Link>
                 </li>
               </ul>
             </li>
-            <li>
+            <li className={s.header__item}>
               {" "}
               <Link href="/blogs" className={s.header__link}>
                 Blog
               </Link>
             </li>
-            <li>
+            <li className={s.header__item}>
               {" "}
               <Link href="/contact-us" className={s.header__link}>
                 Contact Us
@@ -103,11 +99,11 @@ const HeaderComponent = () => {
             </li>
           </ul>
         </nav>
-        <button className={s.header__btnBurger}>
+        <button className={s.header__btn_burger}>
           <IoIosMenu className={s.header__burger} width="18px" height="13px" />
         </button>
-        <button className={s.header__btnHeader}>
-          <p className={s.header__btnText}>Login</p>
+        <button className={s.header__btn_header}>
+          <p className={s.header__btn_text}>Login</p>
         </button>
       </div>
       <hr className={s.header__line}></hr>
