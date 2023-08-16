28bf6e4cd4bd647e13da5382c0e11efe4206bb16
Mike Bakuzov <100297704+Bakuzov@users.noreply.github.com>
2023-08-16 18:14:26 +0300

    init: modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import "./../styles/main.scss";
 import HeaderComponent from "../components/HeaderComponent";
 import "./../styles/main.scss";
--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -1,11 +1,20 @@
+/* use client */
 import Link from "next/link";
 import s from "./HeaderComponent.module.scss";
 import { IoIosMenu } from "react-icons/io";
+import { MdKeyboardArrowDown } from "react-icons/md";
 import Logo from "images/logo.svg";
 import Image from "next/image";
-import classNames from "classnames";
+import React, { useState } from "react";
 
 const HeaderComponent = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isBurgerOpen, setBurgerOpen] = useState(false);
+
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+    setBurgerOpen(!isBurgerOpen);
+  };
   return (
     <header className={s.container}>
       <div className={s.header}>
@@ -51,7 +60,7 @@ const HeaderComponent = () => {
                 Technologies
               </Link>
               <ul className={s.header__sub_menu}>
-                <li >
+                <li>
                   {" "}
                   <Link href="/services/java" className={s.header__link}>
                     Java
@@ -99,9 +108,120 @@ const HeaderComponent = () => {
             </li>
           </ul>
         </nav>
-        <button className={s.header__btn_burger}>
-          <IoIosMenu className={s.header__burger} width="18px" height="13px" />
+        <button className={s.header__btn_burger} onClick={toggleModal}>
+          {isBurgerOpen ? (
+            <span>X</span>
+          ) : (
+            <IoIosMenu
+              className={s.header__burger}
+              width="18px"
+              height="13px"
+            />
+          )}
         </button>
+        {isModalOpen && (
+          <div className={s.header_modal}>
+            <ul className={s.header_modal__list}>
+              <li className={s.header_modal__item}>
+                {" "}
+                <Link href="/services" className={s.header_modal__link}>
+                  <p>Services</p>
+                  <MdKeyboardArrowDown
+                    className={s.header_modal__icon}
+                    width="18px"
+                    height="18px"
+                  />
+                </Link>
+                <ul className={s.header_modal__sub_menu}>git add . 
+                  <li>
+                    {" "}
+                    <Link
+                      href="/services/design"
+                      className={s.header_modal__sublink}
+                    >
+                      Design
+                    </Link>
+                  </li>
+                  <li>
+                    {" "}
+                    <Link
+                      href="/services/development"
+                      className={s.header_modal__link}
+                    >
+                      Development
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/services/apps"
+                      className={s.header_modal__link}
+                    >
+                      Apps
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className={s.header_modal__item}>
+                {" "}
+                <Link href="/projects" className={s.header_modal__link}>
+                  Our Projects
+                </Link>
+              </li>
+              <li className={s.header_modal__item}>
+                {" "}
+                <Link href="/technologies" className={s.header_modal__link}>
+                  Technologies
+                </Link>
+                {/* <ul className={s.header__sub_menu}>
+                <li>
+                  {" "}
+                  <Link href="/services/java" className={s.header__link}>
+                    Java
+                  </Link>
+                </li>
+                <li>
+                  {" "}
+                  <Link href="/services/react" className={s.header__link}>
+                    React.js
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/angular" className={s.header__link}>
+                    Angular
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/vue" className={s.header__link}>
+                    Vue.js
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/node" className={s.header__link}>
+                    Node.js
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/net" className={s.header__link}>
+                    .NET
+                  </Link>
+                </li>
+              </ul> */}
+              </li>
+              <li className={s.header_modal__item}>
+                {" "}
+                <Link href="/blogs" className={s.header_modal__link}>
+                  Blog
+                </Link>
+              </li>
+              <li className={s.header_modal__item}>
+                {" "}
+                <Link href="/contact-us" className={s.header_modal__link}>
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
         <button className={s.header__btn_header}>
           <p className={s.header__btn_text}>Login</p>
         </button>
