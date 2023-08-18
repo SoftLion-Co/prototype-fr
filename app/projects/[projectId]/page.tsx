902d7eaccaeed55d2f17bdff6e7d826b7223ea6c
Yurii <tahkict.yup4ik@gmail.com>
2023-08-18 11:19:51 +0300

    chore: add sections to project page

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -2,13 +2,18 @@ import s from "./page.module.scss";
 import ResultSection from "@/app/sections/project_page/ResultSection";
 import RequestSection from "@/app/sections/project_page/RequestSection";
 import ProjectDescriptionSection from "@/app/sections/project_page/ProjectDescriptionSection";
+import SolutionSection from "@/app/sections/project_page/SolutionSection";
+import BlogSection from "@/app/sections/home_page/BlogSection";
+
 
 const Project = () => {
   return (
     <div>
       <ProjectDescriptionSection />
-      <ResultSection />
       <RequestSection />
+      <SolutionSection />
+      <ResultSection />
+      <BlogSection />
     </div>
   );
 };
