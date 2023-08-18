86fb41bac1475b161890374f9e725caad790bbc0
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-08-18 16:32:34 +0300

    feature: add section chose-us

--- app/sections/technologies_page/ChooseUsSection.tsx
+++ app/sections/technologies_page/ChooseUsSection.tsx
@@ -1,11 +1,49 @@
-import s from "./ChooseUsSection.module.scss"
+import React from "react";
+import s from "./ChooseUsSection.module.scss";
+import jsonData from "../../../data/technologies/data_java.json";
+import classNames from "classnames";
+import Image from "next/image";
+import SvgImage1 from "../../../images/technologies/choose-us/ChooseUs1.svg";
+import SvgImage2 from "../../../images/technologies/choose-us/ChooseUs2.svg";
+import SvgImage3 from "../../../images/technologies/choose-us/ChooseUs3.svg";
+import BigButtonComponent from "@/components/service/BigButtonComponent";
+import HeadingComponent from "@/components/technologies/HeadingComponent";
 
 const ChooseUsSection = () => {
-    return (
-        <section>
-            
-        </section>
-    )
-}
+  const chooseUsData = jsonData[0];
+  const svgImages = [SvgImage1, SvgImage2, SvgImage3];
 
-export default ChooseUsSection
+  return (
+    <section className={classNames(s.our__chose, s.container)}>
+      <div className={s.heading}>
+        <HeadingComponent text="Why you should choose us" color="purple" />
+      </div>
+      <div>
+        {chooseUsData.chooseUsSection.map((item, index) => (
+          <div
+            className={classNames(s.card, {
+              [s.reverseRow]: index % 2 === 1,
+            })}
+            key={index}
+          >
+            {index < 3 && (
+              <div className={s.card__image}>
+                <Image
+                  src={svgImages[index]}
+                  alt="SVG"
+                  className={s.card__svg}
+                />
+              </div>
+            )}
+            <p className={s.card__description}>{item}</p>
+            {index === chooseUsData.chooseUsSection.length - 1 && (
+              <BigButtonComponent />
+            )}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ChooseUsSection;
