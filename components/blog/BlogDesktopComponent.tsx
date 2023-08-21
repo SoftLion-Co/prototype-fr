0b5a469cb94baea860d3b517b647619b4e34b729
zhuvakAnd <125382431+zhuvakAnd@users.noreply.github.com>
2023-08-21 13:17:20 +0300

    refactor: style classnames

--- components/blog/BlogDesktopComponent.tsx
+++ components/blog/BlogDesktopComponent.tsx
@@ -6,10 +6,21 @@ import { Carousel, Embla } from "@mantine/carousel";
 import ArrowLeft from "../../images/navigation/arrow-left.svg";
 import ArrowRight from "../../images/navigation/arrow-right.svg";
 import Image from "next/image";
-import { BlogInterface } from "./BlogInteface";
+
+interface BlogExtendedCardComponentProps {
+  id: number;
+  title: string;
+  authorId: string;
+  author: string;
+  authorIconSrc: string;
+  readingTime: string;
+  text: string;
+  imageSrc: string;
+  tags: string[];
+}
 
 interface BlogDesktopProps {
-  cardsData: BlogInterface[];
+  cardsData: BlogExtendedCardComponentProps[];
 }
 
 const BlogDesktopComponent: React.FC<BlogDesktopProps> = ({ cardsData }) => {
@@ -17,7 +28,7 @@ const BlogDesktopComponent: React.FC<BlogDesktopProps> = ({ cardsData }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   return (
-    <div className={s.blogDesktop}>
+    <div className={s.slider}>
       <Carousel
         getEmblaApi={setEmbla}
         loop
@@ -70,7 +81,7 @@ const BlogDesktopComponent: React.FC<BlogDesktopProps> = ({ cardsData }) => {
         {cardsData.map((x, index) => (
           <>
             {currentSlide === index ? (
-              <div className={s.blogDesktop__extendedCard}>
+              <div className={s.slider__card_extended}>
                 <Carousel.Slide key={index}>
                   <BlogExtendedCardComponent
                     id={x.id}
@@ -90,11 +101,11 @@ const BlogDesktopComponent: React.FC<BlogDesktopProps> = ({ cardsData }) => {
                 className={
                   currentSlide - 1 === index ||
                   (currentSlide === 0 && index === cardsData.length - 1)
-                    ? s.blogDesktop__rolledCardLeft
+                    ? s.slider__card_left
                     : currentSlide + 1 === index ||
                       (currentSlide === cardsData.length - 1 && index === 0)
-                    ? s.blogDesktop__rolledCardRight
-                    : s.blogDesktop__rolledCardDefault
+                    ? s.slider__card_right
+                    : s.slider__card_default
                 }
               >
                 <Carousel.Slide
