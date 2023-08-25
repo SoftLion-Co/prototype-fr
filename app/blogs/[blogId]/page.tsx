23b2fa79a07485c3309f8c786d0d05bc1ba07baf
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-08-25 14:46:43 +0300

    refactor: ArticleSection import

--- app/blogs/[blogId]/page.tsx
+++ app/blogs/[blogId]/page.tsx
@@ -1,6 +1,6 @@
 import s from "./page.module.scss";
 import ImpressionSection from "@/app/sections/article_page/ImpressionSection";
-import { ArticleSection } from "@/app/sections/article_page/ArticleSection";
+import ArticleSection from "@/app/sections/article_page/ArticleSection";
 import InfoNavigationComponent from "@/components/InfoNavigationComponent";
 import OurTeamSetcion from "@/app/sections/home_page/OurTeamSection";
 
