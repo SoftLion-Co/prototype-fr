82876f865add8bd2c39ddec98f5f9c04343e3e2e
Yurii <tahkict.yup4ik@gmail.com>
2023-10-20 21:28:51 +0300

    refactor: base sitemap pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,74 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://softlion.co',
+      url: "https://softlion.co",
       lastModified: new Date(),
     },
     {
-      url: 'https://softlion.co/blog',
+      url: "https://softlion.co/blog",
       lastModified: new Date(),
     },
     {
-      url: 'https://softlion.co/services',
+      url: "https://softlion.co/contact-us",
       lastModified: new Date(),
     },
-  ]
+    {
+      url: "https://softlion.co/enter-code",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/forgot-password",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/new-password",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/privacy-policy",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/projects",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/registration",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/technologies/net",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/technologies/react",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/technologies/java",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/technologies/vue",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/technologies/angular",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://softlion.co/technologies/node",
+      lastModified: new Date(),
+    },
+  ];
 }
