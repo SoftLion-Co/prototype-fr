3093969617750ffc887b45ba3448e81eb190e027
Yurii <tahkict.yup4ik@gmail.com>
2023-10-20 14:12:11 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-    // Налаштування підключення SCSS
-    sassOptions: {
-      includePaths: ["styles"],
-    },
+  // Налаштування підключення SCSS
+  sassOptions: {
+    includePaths: ["styles"],
+  },
 
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: '**',
-        },
-      ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+
+  sitemap: {
+    hostname: "https://softlion.co",
+    generate: true,
+  },
 };
