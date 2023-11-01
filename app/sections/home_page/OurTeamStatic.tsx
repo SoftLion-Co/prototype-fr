1677b9bb73ed3a2be65a58737d5b0d0ac4c28fbf
yuro4kaw <tahkict.yup4ik@gmail.com>
2023-11-01 13:02:31 +0200

    refactor: our team and blog authors chages

--- app/sections/home_page/OurTeamStatic.tsx
+++ app/sections/home_page/OurTeamStatic.tsx
@@ -22,14 +22,14 @@ const OurTeamSetcion = () => {
   }
 
   const response: Props[] = [
-    // {
-    //   id: "1",
-    //   name: "Pavlo Graur",
-    //   position: "CEO",
-    //   avatar:
-    //     "https://media.licdn.com/dms/image/D4E03AQGCS4dmL8ZCLQ/profile-displayphoto-shrink_800_800/0/1697010844966?e=1702512000&v=beta&t=o1zowEeuAk2Y-eW6DL6IrIzkaOMJgeVbSD9uEHIY_lM",
-    //   linkedinUrl: "https://www.linkedin.com/in/paul-graur-07526a247/",
-    // },
+    {
+      id: "1",
+      name: "Pavlo Graur",
+      position: "CEO",
+      avatar:
+        "https://media.licdn.com/dms/image/D4E03AQGCS4dmL8ZCLQ/profile-displayphoto-shrink_800_800/0/1697010844966?e=1702512000&v=beta&t=o1zowEeuAk2Y-eW6DL6IrIzkaOMJgeVbSD9uEHIY_lM",
+      linkedinUrl: "https://www.linkedin.com/in/paul-graur-07526a247/",
+    },
     {
       id: "2",
       name: "Yurii Matvii",
@@ -40,14 +40,6 @@ const OurTeamSetcion = () => {
     },
     {
       id: "3",
-      name: "Dima Klapiychuk",
-      position: "COO",
-      avatar:
-        "https://media.licdn.com/dms/image/D4D03AQHAgP85JknqKA/profile-displayphoto-shrink_800_800/0/1686751374128?e=1698883200&v=beta&t=Ks5_aBaMfSvX5Obq1Xx-XhwMB6J4qD1M79q_M7coCUo",
-      linkedinUrl: "https://www.linkedin.com/in/dima-klapiychuk-22a989268/",
-    },
-    {
-      id: "4",
       name: "Vira Lyzen",
       position: "Head Of Development",
       avatar:
@@ -55,7 +47,7 @@ const OurTeamSetcion = () => {
       linkedinUrl: "https://www.linkedin.com/in/vira-lyzen-a63ba1281/",
     },
     {
-      id: "5",
+      id: "4",
       name: "Tetiana Hlushko",
       position: "UI/UX Designer",
       avatar: AvatarTetiana,
