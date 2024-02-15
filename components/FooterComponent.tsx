0119943aff0865ff706d71d41f60131724842816
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-02-16 00:15:00 +0200

    refactor: change footer style

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -33,79 +33,81 @@ const FooterComponent = () => {
 
   return (
     <footer id="footer" className={s.footer}>
-      <div className={classNames(s.blur, s.container)}>
-        <div className={s.blur_item}></div>
-      </div>
-      <div className={classNames(s.footer__block)}>
-        <div className={s.footer__container}>
-          <div className={s.footer__brand}>
-            <Link href="/">
-              <Image
-                alt="SoftLion"
-                className={s.footer__logo}
-                src={Logo}
-                width={800}
-                height={554}
-              />
-            </Link>
+      <div className={s.footer__contents}>
+        {/* <div className={classNames(s.blur, s.container)}>
+          <div className={s.blur_item}></div>
+        </div> */}
+        <div className={classNames(s.footer__block)}>
+          <div className={s.footer__container}>
+            <div className={s.footer__brand}>
+              <Link href="/">
+                <Image
+                  alt="SoftLion"
+                  className={s.footer__logo}
+                  src={Logo}
+                  width={300}
+                  height={150}
+                />
+              </Link>
 
-            <p className={classNames(s.footer__text, s.footer__copyright)}>
-              ©2023 Copyright SoftLion. All rights reserved.
-            </p>
-          </div>
-        </div>
-
-        <div className={s.footer__box}>
-          <div className={s.footer__links}>
-            <div className={s.footer__routs}>
-              {routes.map((route, index) => (
-                <Link
-                  key={index}
-                  href={route.path}
-                  className={classNames(s.footer__hover, s.footer__text)}
-                >
-                  {route.label}
-                </Link>
-              ))}
+              <p className={classNames(s.footer__text, s.footer__copyright)}>
+                ©2023 Copyright SoftLion. All rights reserved.
+              </p>
             </div>
-            <Link
-              href="/privacy-policy"
-              className={classNames(s.footer__hover, s.footer__text)}
-            >
-              Privacy policy
-            </Link>
           </div>
 
-          <div className={s.footer__contact}>
-            <div className={s.footer__content}>
-              <div className={s.footer__icons}>
-                {socialLinks.map((link, index) => (
+          <div className={s.footer__box}>
+            <div className={s.footer__links}>
+              <div className={s.footer__routs}>
+                {routes.map((route, index) => (
                   <Link
                     key={index}
-                    className={s.footer__hover}
-                    href={link.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
+                    href={route.path}
+                    className={classNames(s.footer__hover, s.footer__text)}
                   >
-                    {link.icon}
+                    {route.label}
                   </Link>
                 ))}
               </div>
-              <a
-                href="mailto:office.softlion@gmail.com"
+              <Link
+                href="/privacy-policy"
                 className={classNames(s.footer__hover, s.footer__text)}
               >
-                office.softlion@gmail.com
-              </a>
+                Privacy policy
+              </Link>
             </div>
-            <div className={s.footer__btn}>
-              <MainButtonComponent
-                color="dark-blue"
-                path="contact-us"
-                className={s.footer__btn_text}
-              >
-                Book Consultation
-              </MainButtonComponent>
+
+            <div className={s.footer__contact}>
+              <div className={s.footer__content}>
+                <div className={s.footer__icons}>
+                  {socialLinks.map((link, index) => (
+                    <Link
+                      key={index}
+                      className={s.footer__hover}
+                      href={link.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {link.icon}
+                    </Link>
+                  ))}
+                </div>
+                <a
+                  href="mailto:office.softlion@gmail.com"
+                  className={classNames(s.footer__hover, s.footer__text)}
+                >
+                  office.softlion@gmail.com
+                </a>
+              </div>
+              <div className={s.footer__btn}>
+                <MainButtonComponent
+                  color="dark-blue"
+                  path="contact-us"
+                  className={s.footer__btn_text}
+                >
+                  Book Consultation
+                </MainButtonComponent>
+              </div>
             </div>
           </div>
         </div>
