005a3eb137f306d67d45df3bed25fe25a4080072
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-03-05 08:59:23 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <meta
-          name="description"
-          content="SoftLion - Your Trusted Partner in IT Services and Consulting. We provide web development, consulting, and technology solutions to drive your business growth."
-        />
+        <meta name="description" content={metadata.description} />
         <meta
           name="keywords"
           content="софтлайон, web-company, shopify, development, outsourcing, programming outsourcing"
