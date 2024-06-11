bf4e5ef1e12a763c035aa50e3ca7af16f9a88612
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-06-11 14:33:04 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const Home = () => {
       <OurServicesSection />
       <OurProjectsSection />
       <BlogSection />
-      <OurTeamStatic />
+      {/* <OurTeamStatic /> */}
       <ContactUsSection className={s.main__contactUs} />
     </React.Fragment>
   );
